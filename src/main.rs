77f00d26723eb6b0f6b09988c9cdd80d8a457ee8
marcelca02 <marcel.calleja@estudiantat.upc.edu>
2023-11-04 17:33:54 +0100

    http OK response

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,10 @@ fn main() {
         match stream {
             Ok(_stream) => {
                 println!("accepted new connection");
+                let response = "HTTP/1.1 200 OK\r\n\r\n";
+                stream
+                    .write_all(response.as_bytes())
+                    .expect("failed to write to stream");
             }
             Err(e) => {
                 println!("error: {}", e);
